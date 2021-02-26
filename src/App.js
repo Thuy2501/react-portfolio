4fc70b441682b850285a344225dca1dcefb6208e
Thuy2501 <2501ngocthuy@gmail.com>
2021-02-26 09:26:12 +0700

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div><h2>Portfolio project...</h2></div>
+    <div>
+      <Navbar/>
+    </div>
   );
 }
 
